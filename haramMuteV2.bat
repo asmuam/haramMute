c2a762e03069a563772644a46fc7ec1130f5ffdf
asmuammal <100674458+asmuam@users.noreply.github.com>
2026-07-09 12:15:33 +0700

    feat(config): update haramMuteV2.bat to run script with --env

--- haramMuteV2.bat
+++ haramMuteV2.bat
@@ -2,12 +2,12 @@
 title HaramMute V2 (Demucs Light)
 echo Menjalankan HaramMute V2...
 echo.
-set /p choiceVal="Gunakan parameter default (Chunk: 0.3s)? [Y/N, default Y]: "
+set /p choiceVal="Gunakan parameter default dari .env? [Y/N, default Y]: "
 if /i "%choiceVal%"=="N" (
     echo Menjalankan mode interaktif...
     python haramMuteV2.py
 ) else (
-    echo Menjalankan dengan parameter default...
-    python haramMuteV2.py -c 0.3
+    echo Menjalankan dengan parameter default dari .env...
+    python haramMuteV2.py --env
 )
 pause
